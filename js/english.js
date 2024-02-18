693b0b44de94fcb20db7e3d3294570d429cf1353
Alexander Golin <agolin95@gmail.com>
2024-02-18 13:32:31 -0500

    unidirectional ascii translation from english

--- js/english.js
+++ js/english.js
@@ -1,26 +1,46 @@
 $(function () {
     $("#english").keyup(function () {
         const englishMessage = $(this).text().trim();
-        let morseMessage = "";
-        for (let i = 0; i < englishMessage.length; i++) {
-            const englishLetter = englishMessage[i].toUpperCase();
-            const morseLetter = morseDict[englishLetter];
-            if (morseLetter != undefined) {
-                morseMessage += morseLetter + " ";
-            }
-        }
-        $("#morse").text(morseMessage);
+        toMorse(englishMessage);
+        toAscii(englishMessage);
     });
 
     $("#speak").click(function () {
-        speak();
+        speak($("#english").text().trim());
+    });
+
+    $("#play-ascii").click(function () {
+        speak($("#ascii").text().trim());
     });
 });
 
-function speak() {
+function toMorse(text) {
+    let morseMessage = "";
+    for (let i = 0; i < text.length; i++) {
+        const letter = text[i].toUpperCase();
+        const morseLetter = morseDict[letter];
+        if (morseLetter != undefined) {
+            morseMessage += morseLetter + " ";
+        }
+    }
+    $("#morse").text(morseMessage);
+}
+
+function toAscii(text) {
+    let asciiMessage = "";
+    for (let i = 0; i < text.length; i++) {
+        const letter = text[i];
+        const asciiLetter = asciiDict[letter];
+        if (asciiLetter != undefined) {
+            asciiMessage += asciiLetter + " ";
+        }
+    }
+    $("#ascii").text(asciiMessage);
+}
+
+function speak(text) {
     const synth = window.speechSynthesis;
-    const englishText = $("#english").text().trim();
-    const utterance = new SpeechSynthesisUtterance(englishText);
+    const utterance = new SpeechSynthesisUtterance(text);
     if (synth.speaking) {
         synth.cancel();
     }
