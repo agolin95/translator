d2b68a2760bd9a665f8c2bbf4ba0550f042c8a81
Alexander Golin <agolin95@gmail.com>
2024-02-18 12:41:03 -0500

    separate speaking and morse playback

--- main.js
+++ main.js
@@ -39,35 +39,35 @@ $(function () {
     $("#speak").click(function () {
         speak();
     });
+
+    $("#playMorse").click(function () {
+        const morseText = $("#morse").text().trim();
+        playMorse(morseText);
+    });
 });
 
 function speak() {
+    const synth = window.speechSynthesis;
     const englishText = $("#english").text().trim();
-    const morseText = $("#morse").text().trim();
     const utterance = new SpeechSynthesisUtterance(englishText);
-    utterance.onend = function (event) { playMorse(morseText); }
-    window.speechSynthesis.speak(utterance);
+    if (synth.speaking) {
+        synth.cancel();
+    }
+    synth.speak(utterance);
 }
 
 function playMorse(morse) {
     dotlength = 50;
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var oscillator = audioCtx.createOscillator();
-    var gainNode = audioCtx.createGain();
-    oscillator.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-    gainNode.gain.value = 0;
-    oscillator.type = 'sine';
-    oscillator.frequency.value = 440;
-    oscillator.start();
+    const audioNodes = createAudioNodes();
+    audioNodes.osc.start();
     let duration = 0;
     switch (morse[0]) {
         case ".":
-            gainNode.gain.value = 0.1;
+            audioNodes.gain.value = 0.1;
             duration = dotlength;
             break;
         case "-":
-            gainNode.gain.value = 0.1;
+            audioNodes.gain.value = 0.1;
             duration = dotlength * 3;
             break;
         case " ":
@@ -78,13 +78,25 @@ function playMorse(morse) {
             break;
     }
     setTimeout(() => {
-        oscillator.stop();
+        audioNodes.osc.stop();
         setTimeout(() => {
             if (morse.length > 1) {
                 playMorse(morse.substring(1))
             }
         }, dotlength * 1.2); // Constant space between dots and dashes
     }, duration); // Duration of tone or silence
+}
+
+function createAudioNodes() {
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var oscillator = audioCtx.createOscillator();
+    var gainNode = audioCtx.createGain();
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    gainNode.gain.value = 0;
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 440;
+    return { "osc": oscillator, "gain": gainNode.gain };
 }
 
 function getKeyByValue(object, value) {
